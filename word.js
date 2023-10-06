fb977bc74788668d1d567046f97406f4d484c5a9
logasubramanism <smlogasubramani@gmail.com>
2023-10-06 23:00:09 +0530

    categorised

--- word.js
+++ word.js
@@ -2,9 +2,12 @@
 
 const WordExtractor = require("word-extractor"); 
 const extractor = new WordExtractor();
-const extracted = extractor.extract("funding_req.docx");
+const extracted = extractor.extract("Resume.docx");
 
 extracted.then(function(doc)
  {
      console.log(doc.getBody());
- });
+ }
+ );
+
+ //need to add router file 
