32cce7009b70599dfde6ac55181dfd0b87d1fd39
logasubramanism <smlogasubramani@gmail.com>
2023-11-20 11:31:38 +0530

    added the cors package

--- Backend/main.js
+++ Backend/main.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app =  express();
+const cors = require('cors');
 const fileroute  = require('./src/routes');
 
-app.use('/',fileroute);
+
+app.use(cors()); 
 app.use(express.json());
+app.use('/',fileroute);
+
+
 
 app.listen('3000',()=>{
     console.log('server running on port 3000');
